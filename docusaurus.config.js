ce9c8a858c4037c2dd2c8607112dafa477e6baca
Yetty <88237957+theyetty@users.noreply.github.com>
2025-01-20 11:48:14 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/theyetty/reforger-docs/tree/main/docs/',
+            'https://github.com/theyetty/reforger-docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -57,7 +57,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/theyetty/reforger-docs/tree/main/docs/',
+            'https://github.com/theyetty/reforger-docs/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
